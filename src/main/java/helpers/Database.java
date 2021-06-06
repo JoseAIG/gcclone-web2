1e5c48f9b4c40fddf27c795bbc774dd063b33ac9
JoseAIG <inciartegj@gmail.com>
2021-06-06 16:30:10 -0400

    conexion sin properties
    prueba de conexion sin properties

--- src/main/java/helpers/Database.java
+++ src/main/java/helpers/Database.java
@@ -21,26 +21,26 @@ public class Database {
 //	private String userDB = "postgres";
 //	private String passDB = "masterkey";
 	//REMOTO EN HEROKU
-//	private String dbName = "dag91rnv1gm978";
-//	private String urlDB = "jdbc:postgresql://ec2-3-215-57-87.compute-1.amazonaws.com:5432/" + this.dbName;
-//	private String userDB = "dtqgnhgnhrsctt";
-//	private String passDB = "12afd4794a6db97bb6cd443726c7f81888b8cfefc66c053d6174d472329b4c1c";
+	private String dbName = "dag91rnv1gm978";
+	private String urlDB = "jdbc:postgresql://ec2-3-215-57-87.compute-1.amazonaws.com:5432/" + this.dbName;
+	private String userDB = "dtqgnhgnhrsctt";
+	private String passDB = "12afd4794a6db97bb6cd443726c7f81888b8cfefc66c053d6174d472329b4c1c";
 	
-	private String dbName;
-	private String urlDB;
-	private String userDB;
-	private String passDB;
+//	private String dbName;
+//	private String urlDB;
+//	private String userDB;
+//	private String passDB;
 	
 	//CONSTRUCTOR
 	private Database(){
 		try {
 			//LEER PROPIEDADES
-			properties.load(this.getClass().getResourceAsStream("/properties/db.properties"));
-			
-			dbName = properties.getProperty("dbName");
-			urlDB = properties.getProperty("urlDB") + this.dbName;
-			userDB = properties.getProperty("userDB");
-			passDB = properties.getProperty("passDB");
+//			properties.load(this.getClass().getResourceAsStream("/properties/db.properties"));
+//			
+//			dbName = properties.getProperty("dbName");
+//			urlDB = properties.getProperty("urlDB") + this.dbName;
+//			userDB = properties.getProperty("userDB");
+//			passDB = properties.getProperty("passDB");
 			
 			//ESTABLECER CONEXION
 			Class.forName(driverDB);
